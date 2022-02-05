a3255f4f6fee35069f1de3ad000418f62bdcc916
Faeune <coleman14532@gmail.com>
2022-02-05 13:01:37 -0500

    Update for Challenge

---
+++ Create with Code/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 offset = new Vector3(30, 0, 0);
+    public Quaternion cameraRotation = new Quaternion(0, -90, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+        transform.rotation = cameraRotation;
+    }
+}
